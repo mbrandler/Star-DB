8bdadd924644bd73d7c73ed6ebb570d8a0c77351
mbrandler <marya.brandler@yandex.ru>
2020-06-09 19:01:59 +0300

    Updates

---
+++ precache-manifest.c4fece0cd792e72c490fe9124d21005b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5c900082b5cf9f3335b7f32d140151",
+    "url": "/Star-DB/index.html"
+  },
+  {
+    "revision": "378e41b96d54943c008c",
+    "url": "/Star-DB/static/css/main.37231293.chunk.css"
+  },
+  {
+    "revision": "7bedf910b654e111d357",
+    "url": "/Star-DB/static/js/2.4fa9d8a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Star-DB/static/js/2.4fa9d8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378e41b96d54943c008c",
+    "url": "/Star-DB/static/js/main.67251a06.chunk.js"
+  },
+  {
+    "revision": "10ce4c0f288c32f8df83",
+    "url": "/Star-DB/static/js/runtime-main.922184f6.js"
+  }
+]);
